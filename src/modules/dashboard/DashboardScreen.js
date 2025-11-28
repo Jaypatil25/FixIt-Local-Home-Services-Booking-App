ee9567287e814fb78acc29e4161359cbf2295e4e
Suryansh0910 <suryansh0910sg@gmail.com>
2025-11-29 01:39:04 +0530

    added js file and required importing

---
+++ src/modules/dashboard/DashboardScreen.js
@@ -0,0 +1,4 @@
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { providers } from "../../data/providers";
