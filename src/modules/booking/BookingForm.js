937ccea9f896457e24b1e23e88c1065269e9ebf6
Archis009 <vedvadnere@gmail.com>
2025-11-29 01:08:13 +0530

    added Async-Storage

--- src/modules/booking/BookingForm.js
+++ src/modules/booking/BookingForm.js
@@ -1,2 +1,3 @@
 import { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView, Alert } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage"; 
