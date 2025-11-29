f1601cae5cf886f5bcf0728b0dd360c6ab1bb46f
Archis009 <vedvadnere@gmail.com>
2025-11-29 10:43:50 +0530

    Importing Components

---
+++ src/modules/favourites/FavoritesScreen.js
@@ -0,0 +1,4 @@
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, SafeAreaView, FlatList } from "react-native";
+import { providers } from "../../data/providers";
+import ProviderCard from "../providers/ProviderCard";
